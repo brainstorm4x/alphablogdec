0f1b9b2207b4f99360b827dc8cf1e2c835354795
Mukesh <brainstorm4x@gmail.com>
2022-12-22 18:42:52 +0530

    create articles table

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,8 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_12_18_133719) do
   create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
